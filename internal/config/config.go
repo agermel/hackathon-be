f9cdd3d3060df79ffe61ccdd951286aa643cfed3
agermel <agermel@mails.ccnu.edu.cn>
2025-05-24 22:26:27 +0800

    ✨ 连接数据库

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,34 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"fmt"
+
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
+
+	Auth struct {
+		AccessSecret string
+		AccessExpire int64
+	}
+
+	MySQL struct {
+		Name     string
+		User     string
+		Password string
+		Host     string
+		Port     int
+	}
+}
+
+func (c Config) DSN() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		c.MySQL.User,
+		c.MySQL.Password,
+		c.MySQL.Host,
+		c.MySQL.Port,
+		c.MySQL.Name,
+	)
 }
--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -2,14 +2,19 @@ package svc
 
 import (
 	"hackathon_be/internal/config"
+	"hackathon_be/internal/model"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	ScoreModel model.ScoreModel
 }
 
-func NewServiceContext(c config.Config) *ServiceContext {
+func NewServiceContext(c config.Config, conn sqlx.SqlConn) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		ScoreModel: model.NewScoreModel(conn),
 	}
 }
